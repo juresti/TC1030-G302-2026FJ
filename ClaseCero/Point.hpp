6b2b09e3fd0162f640835317125773972909671d
Jorge Adolfo Ramírez Uresti <juresti@tec.mx>
2026-05-07 15:13:42 -0600

    Ejemplo creando un proyecto para clase

---
+++ ClaseCero/Point.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Dr.Jorge on 07/05/26.
+//
+
+#ifndef TC1030_POINT_HPP
+#define TC1030_POINT_HPP
+
+class Point {
+    int x,y;
+public:
+    Point();
+    int getX();
+    int getY();
+};
+
+#endif //TC1030_POINT_HPP
---
+++ ClaseCero/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Dr.Jorge on 07/05/26.
+//
+#include <iostream>
+#include "Point.hpp"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    Point p1 {};
+    cout << p1.getX() << endl;
+    cout << p1.getY() << endl;
+}
