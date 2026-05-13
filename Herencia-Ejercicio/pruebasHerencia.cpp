6ec007fd03cc50bcbe30e3fc7af3a132d52cf856
Jorge Adolfo Ramírez Uresti <juresti@tec.mx>
2026-05-13 16:07:17 -0600

    Carpeta para ejercicio de Herencia

---
+++ Herencia-Ejercicio/pruebasHerencia.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Dr.Jorge on 13/05/26.
+//
+
+#include <iostream>
+#include <ostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    cout << "Hola Herencia!" << endl;
+}
